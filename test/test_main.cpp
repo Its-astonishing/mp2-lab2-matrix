0e26ea16b33efca9c704250259942b674d2c9e88
Grigory Kapustin <79307199926@yandex.ru>
2018-10-10 13:43:37 +0300

    Готово, осталось форматирование

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -3,9 +3,6 @@
 
 int main(int argc, char **argv)
 {
-    //TVector<int> test(100, 0);
-    //std::cout << sizeof(test);
-    //system("pause");
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
--- test/test_tvector.cpp
+++ test/test_tvector.cpp
@@ -4,34 +4,34 @@
 
 TEST(TVector, can_create_vector_with_positive_length)
 {
-  ASSERT_NO_THROW(TVector<int> v(5));
+    ASSERT_NO_THROW(TVector<int> v(5));
 }
 
 TEST(TVector, cant_create_too_large_vector)
 {
-  ASSERT_ANY_THROW(TVector<int> v(MAX_VECTOR_SIZE + 1));
+    ASSERT_ANY_THROW(TVector<int> v(MAX_VECTOR_SIZE + 1));
 }
 
 TEST(TVector, throws_when_create_vector_with_negative_length)
 {
-  ASSERT_ANY_THROW(TVector<int> v(-5));
+    ASSERT_ANY_THROW(TVector<int> v(-5));
 }
 
 TEST(TVector, throws_when_create_vector_with_negative_startindex)
 {
-  ASSERT_ANY_THROW(TVector<int> v(5, -2));
+    ASSERT_ANY_THROW(TVector<int> v(5, -2));
 }
 
 TEST(TVector, can_create_copied_vector)
 {
-  TVector<int> v(10);
+    TVector<int> v(10);
 
-  ASSERT_NO_THROW(TVector<int> v1(v));
+    ASSERT_NO_THROW(TVector<int> v1(v));
 }
 
 TEST(TVector, copied_vector_is_equal_to_source_one)
 {
-    TVector<int> bf1(10),expBf(10);
+    TVector<int> bf1(10), expBf(10);
     for (int i = 0; i < 10; i++)
     {
         bf1[i] = i;
@@ -46,29 +46,29 @@ TEST(TVector, copied_vector_has_its_own_memory)
     TVector<int> bf1(10, 0);
     TVector<int> expBf(bf1);
     bf1[2] = 1;
-    EXPECT_NE(expBf,bf1);
+    EXPECT_NE(expBf, bf1);
 }
 
 TEST(TVector, can_get_size)
 {
-  TVector<int> v(4);
+    TVector<int> v(4);
 
-  EXPECT_EQ(4, v.GetSize());
+    EXPECT_EQ(4, v.GetSize());
 }
 
 TEST(TVector, can_get_start_index)
 {
-  TVector<int> v(4, 2);
+    TVector<int> v(4, 2);
 
-  EXPECT_EQ(2, v.GetStartIndex());
+    EXPECT_EQ(2, v.GetStartIndex());
 }
 
 TEST(TVector, can_set_and_get_element)
 {
-  TVector<int> v(4);
-  v[0] = 4;
+    TVector<int> v(4);
+    v[0] = 4;
 
-  EXPECT_EQ(4, v[0]);
+    EXPECT_EQ(4, v[0]);
 }
 
 TEST(TVector, throws_when_set_element_with_negative_index)
@@ -136,19 +136,19 @@ TEST(TVector, compare_equal_vectors_return_true)
     bf2[0] = 1;
     bf2[1] = 2;
     bf2[2] = 3;
-    EXPECT_EQ(1, bf1 == bf2);
+    ASSERT_TRUE(bf1 == bf2);
 }
 
 TEST(TVector, compare_vector_with_itself_return_true)
 {
     TVector<int> bf1(3);
-    EXPECT_EQ(1, bf1 == bf1);
+    ASSERT_TRUE(bf1 == bf1);
 }
 
 TEST(TVector, vectors_with_different_size_are_not_equal)
 {
     TVector<int> bf1(3), bf2(10);
-    EXPECT_EQ(0, bf1 == bf2);
+    ASSERT_FALSE(bf1 == bf2);
 }
 
 TEST(TVector, can_add_scalar_to_vector)
@@ -188,11 +188,11 @@ TEST(TVector, can_add_vectors_with_equal_size)
 {
     const int size = 10;
     TVector<int> bf1(size), bf2(size), expBf(size);
-    for (int i = 0; i < size; i++) 
+    for (int i = 0; i < size; i++)
     {
         bf1[i] = i;
         bf2[i] = i + 1;
-        expBf[i] = 2*i + 1;
+        expBf[i] = 2 * i + 1;
     }
     EXPECT_EQ(expBf, bf1 + bf2);
 }
@@ -208,7 +208,7 @@ TEST(TVector, can_subtract_vectors_with_equal_size)
 {
     const int size = 10;
     TVector<int> bf1(size), bf2(size), expBf(size);
-    for (int i = 0; i < size; i++) 
+    for (int i = 0; i < size; i++)
     {
         bf1[i] = i;
         bf2[i] = i + 1;
